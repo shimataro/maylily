50e4d1ffe432b26a14d1630859c639eb9f40c6c6
shimataro <shimataro@zelkova.cc>
2019-10-22 16:39:08 +0900

    Feature/scripts (#54)
    * * move scripts to `scripts` directory
    
    * * add PRODUCT_NAME

--- scripts/create-release-branch.sh
+++ scripts/create-release-branch.sh
@@ -6,7 +6,8 @@
 BASE_BRANCH="develop"
 
 PRODUCT_NAME="MayLily"
-URL_PRODUCT="https://github.com/shimataro/maylily"
+PACKAGE_NAME="maylily"
+URL_PRODUCT="https://github.com/shimataro/${PACKAGE_NAME}"
 URL_REPOSITORY="${URL_PRODUCT}.git"
 URL_COMPARE="${URL_PRODUCT}/compare"
 URL_RELEASE="${URL_PRODUCT}/releases/new"
@@ -23,7 +24,7 @@ COLOR_SELECT="\e[1;32m"
 COLOR_RESET="\e[m"
 
 function main() {
-	cd `dirname ${0}`
+	cd $(dirname ${0})/..
 
 	if [ $# -lt 1 ]; then
 		usage
@@ -144,6 +145,7 @@ function finish() {
 	local VERSION=$1
 	local BRANCH=$2
 	local TAG=$3
+	local TARGET_BRANCH="v${VERSION%%[!0-9]*}"
 
 	echo -e "
 Branch ${COLOR_BRANCH}${BRANCH}${COLOR_RESET} has been created.
@@ -156,13 +158,13 @@ Remaining processes are...
 3. Create a pull-request: ${COLOR_BRANCH}${BRANCH}${COLOR_RESET} to ${COLOR_BRANCH}${BASE_BRANCH}${COLOR_RESET}
 	${URL_COMPARE}/${BASE_BRANCH}...${BRANCH}
 	select ${COLOR_SELECT}Squash and merge${COLOR_RESET}
-4. Create a pull-request: ${COLOR_BRANCH}${BASE_BRANCH}${COLOR_RESET} to ${COLOR_BRANCH}master${COLOR_RESET}
-	${URL_COMPARE}/master...${BASE_BRANCH}
+4. Create a pull-request: ${COLOR_BRANCH}${BASE_BRANCH}${COLOR_RESET} to ${COLOR_BRANCH}${TARGET_BRANCH}${COLOR_RESET}
+	${URL_COMPARE}/${TARGET_BRANCH}...${BASE_BRANCH}
 	select ${COLOR_SELECT}Create a merge commit${COLOR_RESET}
 5. Create a new release
 	${URL_RELEASE}
 	Tag version: ${COLOR_INPUT}${TAG}${COLOR_RESET}
-	Target: ${COLOR_INPUT}master${COLOR_RESET}
+	Target: ${COLOR_INPUT}${TARGET_BRANCH}${COLOR_RESET}
 	Release title: ${COLOR_INPUT}${PRODUCT_NAME} ${VERSION} released${COLOR_RESET}
 	Description this release: (copy and paste CHANGELOG.md)
 6. Post processing
